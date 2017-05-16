da4f4b8675eedca983fe31411ab5f58de06aad4c
Sheyda Rezaei <sheydarezaee@gmail.com>
2017-05-16 11:27:58 -0700

    Implement Underbar up to filter

--- src/underbar.js
+++ src/underbar.js
@@ -7,6 +7,7 @@
   // seem very useful, but remember it--if a function needs to provide an
   // iterator when the user does not pass one in, this will be handy.
   _.identity = function(val) {
+    return val;
   };
 
   /**
@@ -37,6 +38,7 @@
   // Like first, but for the last elements. If n is undefined, return just the
   // last element.
   _.last = function(array, n) {
+    return n === undefined ? array[array.length - 1] : !n ? [] : array.slice(-1 * n);
   };
 
   // Call iterator(value, key, collection) for each element of collection.
@@ -45,6 +47,20 @@
   // Note: _.each does not have a return value, but rather simply runs the
   // iterator function over each item in the input collection.
   _.each = function(collection, iterator) {
+    if(Array.isArray(collection)) {
+      for (let i = 0; i < collection.length; i++) {
+        iterator(collection[i], i, collection);
+      }
+    } else  {
+      for (let key in collection) {
+        if(!collection.hasOwnProperty(key)) {
+          continue;
+        }
+        iterator(collection[key], key, collection);
+      }
+    }
+
+
   };
 
   // Returns the index at which value can be found in the array, or -1 if value
@@ -55,17 +71,25 @@
     // it uses the iteration helper `each`, which you will need to write.
     var result = -1;
 
+   // debugger
     _.each(array, function(item, index) {
       if (item === target && result === -1) {
         result = index;
       }
     });
-
+    console.log('result', result)
     return result;
   };
 
   // Return all elements of an array that pass a truth test.
   _.filter = function(collection, test) {
+    let result = [];
+      _.each(collection, value => {
+        if(test(value)) {
+          result.push(value);
+        }
+      });
+    return result;
   };
 
   // Return all elements of an array that don't pass a truth test.
